965196e4da85048610f21085b8683896e8611237
bradromney <brad.romney@gmail.com>
2020-05-26 22:18:00 -0600

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 
 class QuizQuestion extends Component {
+    render() {
+        return (
+            <div>
+            <main>
+                <section>
+                    <p></p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                         
+                    </ul>
+                </section>
+            </main>
+            </div>
+        )
+    }
   }
   
   export default QuizQuestion

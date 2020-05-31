2a324549e7a4485715ab926518e34f6f88d561eb
bradromney <brad.romney@gmail.com>
2020-05-31 16:04:01 -0600

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,20 +11,14 @@ class Quiz extends Component {
   }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
-    if (isQuizEnd) {
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-            </div>
-        )  
-    } else {
-        return (
-            <div>
-                <QuizEnd />
+                {isQuizEnd
+                    ? <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                    : <QuizEnd />
+                }
             </div>
         )
-  
-    }
   }
 }
 

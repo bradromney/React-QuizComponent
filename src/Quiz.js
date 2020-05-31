3f30649acebfa80b78f0e8a073478dd74dd2c317
bradromney <brad.romney@gmail.com>
2020-05-31 16:01:01 -0600

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
   }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
-    if isQuizEnd {
+    if (isQuizEnd) {
         return (
             <div>
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
